0f31af691d65774cc539f329bd8fabee2bee59f1
shahipra974 <160876928+shahipra974@users.noreply.github.com>
2024-05-21 20:32:45 +0200

    Update setup.py

--- Complexify/setup.py
+++ Complexify/setup.py
@@ -5,6 +5,15 @@ setup(
     version = "1.0.0",
     author = "Peri Yerlikaya, Shahina Chopra",
     author_email = "peri.yerlikaya@epfl.ch, shahina.chopra@epfl.ch",
+    install_requires=[
+     "rdkit",
+     "streamlit",
+     "py3Dmol",
+     "numpy",
+     "pandas",
+     "stmol",
+     "matplotlib"
+]
     description = "streamlit page to navigate a coordination complex database",
     url = 'https://github.com/yerlikayaperi/Project_Complexify',
     packages =find_packages(),
@@ -22,5 +31,5 @@ setup(
     ],
     python_require= '>=3.6',
     license= "MIT",
-    },
+    
 )
